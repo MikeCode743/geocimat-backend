d5728c58569d0caf93e5e27bd670a98ca9faec1d
mikehdz743@gmail.com <mikehdz743@gmail.com>
2021-02-18 23:15:17 -0600

    bug in calendar and visitstate

--- resources/views/geocimat/index.blade.php
+++ resources/views/geocimat/index.blade.php
@@ -1,1 +1,23 @@
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>geocimat-v2</title><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css"><link href="/css/about.72114b78.css" rel="prefetch"><link href="/js/about.a1007bd7.js" rel="prefetch"><link href="/css/chunk-vendors.153f271d.css" rel="preload" as="style"><link href="/js/app.cf885e27.js" rel="preload" as="script"><link href="/js/chunk-vendors.b8e376f4.js" rel="preload" as="script"><link href="/css/chunk-vendors.153f271d.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but geocimat-v2 doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div><script src="/js/chunk-vendors.b8e376f4.js"></script><script src="/js/app.cf885e27.js"></script></body></html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>geocimat-v2</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
+    <link href="/css/about.72114b78.css" rel="prefetch">
+    <link href="/js/about.4fddc2dd.js" rel="prefetch">
+    <link href="/css/chunk-vendors.153f271d.css" rel="preload" as="style">
+    <link href="/js/app.47439f01.js" rel="preload" as="script">
+    <link href="/js/chunk-vendors.b8e376f4.js" rel="preload" as="script">
+    <link href="/css/chunk-vendors.153f271d.css" rel="stylesheet">
+</head>
+<body><noscript><strong>We're sorry but geocimat-v2 doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
+    <div id="app"></div>
+    <script src="/js/chunk-vendors.b8e376f4.js"></script>
+    <script src="/js/app.47439f01.js"></script>
+</body>
+</html>
